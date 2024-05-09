699916590a847e6f7bfc2f627f95d940a7239694
JernejSlo <jernejkoprivnikar.slo@gmail.com>
2024-05-09 18:22:26 +0200

    kullback func

--- Kullback-Leibler-distance.py
+++ Kullback-Leibler-distance.py
@@ -1,3 +1,4 @@
+import math
 from random import random
 import numpy as np
 
@@ -8,8 +9,13 @@ distribution = {
     "d": 1/16,
     "e": 1/16
 }
-
-
+distribution2 = {
+    "a": 1/16,
+    "b": 1/4,
+    "c": 1/8,
+    "d": 1/2,
+    "e": 1/16
+}
 def generate_random_sequence_with_distribution(distribution, length):
     # Extract the keys (characters) and their corresponding probabilities from the distribution dictionary
     characters = list(distribution.keys())
@@ -20,9 +26,18 @@ def generate_random_sequence_with_distribution(distribution, length):
 
     return ''.join(sequence)  # Convert array to string for easier use
 
-    # Example usage
+def kullback_leibler_distance(p,q,sequence):
+    KL_divergence = 0
+    character_array = list(sequence)
+
+    for char in character_array:
+        KL_divergence += p[char] * math.log2(p[char]/q[char])
+
+    return KL_divergence
 
 
-sequence_length = 20
+sequence_length = 1000
 random_sequence = generate_random_sequence_with_distribution(distribution, sequence_length)
-print(random_sequence)
+
+KL_divergence = kullback_leibler_distance(distribution,distribution2,random_sequence)
+print(KL_divergence)
